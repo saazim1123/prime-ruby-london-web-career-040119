8e3fec364c12cbb76f2841ec8399ed5941656315
Adnan Azim <saazim1123@gmail.com>
2019-03-10 23:07:05 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,32 @@
 # Add  code here!
+
+=begin
+ Prime number is a natural number greater than one that cannot be formed by multiplying
+ two smaller numbers. For e.g 5 is a prime number since no 2 numbers can multiply a product of 5
+=end 
+
+
+def prime?(number)
+  if number < 2 
+    return false
+  else 
+    (2..number - 1).each do |x|
+      if number % x == 0 
+        return false
+      end
+    end
+    return true
+  end
+end
+
+
+
+
+
+
+
+puts prime?(9)
+
+
+
+
